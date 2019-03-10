eb37db92d4cd7d4141d8712fc006e9abdafc7bdc
Mattias Park <mattiaspark@uvic.ca>
2019-03-09 18:17:23 -0800

    bruh

--- a2/coding.py
+++ a2/coding.py
@@ -2,14 +2,28 @@ import sys
 import os
 #from tokenize import tokenize, STRING
 
+def write_magic_number(mtf):
+	pass
 
 def encode_main():
-    print("encode")
+
+	# Get output filename, create output file with .mtf
+	filename = sys.argv[1].split(".")[0]
+	o = open(filename + ".mtf")
+	wordlist = []
 
     while True:
          try:
-            line = (i.readline().split())
+            line = [i.readline().split()]
             # Read and tokenize a line
+            for word in line:
+            	# do the stuff
+            	if !(word in wordlist):
+            		wordlist.insert(0, word) # Prepend word onto wordlist
+            		print(len(wordlist) + " " + "word")
+            	else: 
+            		print(wordlist.index(word) + 1)
+
          except StopIteration:
             # Stop if there's nothing left
             break 
