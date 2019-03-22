330775e106f91aac5edf7216ff32889571708d18
Mattias Park <mattiaspark@uvic.ca>
2019-03-21 17:53:29 -0700

    almost done

---
+++ a3/coding2.py
@@ -0,0 +1,241 @@
+# Mattias Park
+# V00897015
+# SENG 265 Assignment 2
+
+import sys
+import os
+import time
+import binascii
+
+MAGIC_BYTEARRAY = bytearray([0xba,0x5e,0xba,0x12])
+MAGIC_BYTEARRAY_OLD = bytearray([0xba,0x5e,0xba,0x11])
+
+def write_magic_number(mtf):
+    # 0xba  0x5e 0xba 0x11
+    mtf.write(MAGIC_BYTEARRAY)
+
+def encode(filename):
+
+    def write_mtf_symbols(n):
+        if n < 121:
+            # case 1: 1-120
+            o.write(bytes([128 + n])) # write mtf symbol
+        elif n > 120 and n < 376:
+            #case 2: 121 - 375
+            o.write(bytes([121 + 128]))
+            o.write(bytes([n - 121]))   
+        elif n > 375:
+            #case 3: 376 - 65912
+            o.write(bytes([122 + 128]))
+            o.write(bytes([(n - 376) // 256]))
+            o.write(bytes([(n - 376) % 256]))
+
+    try:
+        i = open(filename)
+        file_base_name = filename.split(".")[0]
+        o = open(file_base_name + ".mtf", "wb")
+    except:
+        print("I/O error! probably")
+        sys.exit()
+
+    write_magic_number(o)
+    wordlist = []
+
+    for line in i:
+            splitline = line.split()
+            # Tokenize the line
+            for word in splitline:
+                # do the stuff
+                if word in wordlist:
+                    # Word already in list: fetch index, move to front
+                    n = wordlist.index(word) + 1
+                    write_mtf_symbols(n)
+                    wordlist.remove(word)
+                    wordlist.insert(0, word)
+                else: 
+                    # Word doesn't appear: add to list
+                    wordlist.insert(0, word)
+                    n = len(wordlist) # seems like a hack but hey, it works
+                    write_mtf_symbols(n)
+                    o.write(word.encode('ascii')) # Write the word in the mtf
+            o.write('\n'.encode('ascii'))
+
+    i.close()
+    o.close()
+    sys.exit()
+
+
+def decode(filename):
+
+    def check_if_new_word(case):
+        pass
+
+    # Open input, output files
+    try:
+        i = open(filename, 'rb')
+        file_base_name = filename.split(".")[0]
+        o = open(file_base_name + ".txt", "w+")
+    except:
+        print("I/O Error! probably")
+        sys.exit()
+
+    # Check for magic num
+    magicnum = i.read(4)
+    if magicnum != MAGIC_BYTEARRAY and magicnum != MAGIC_BYTEARRAY_OLD:
+        print("Not a valid mtf file- check the magic number?")
+        sys.exit()
+
+    wordlist = []
+    nextbyte = i.read(1)
+
+    while nextbyte != b'':
+        # First byte will be mtf symbol byte, and loop invariant should make
+        # this land on such a byte in each loop
+
+
+        coded_int = int.from_bytes(nextbyte, sys.byteorder)
+        print("Words: " + str(len(wordlist)))
+
+        # Deal with newlines
+        if nextbyte == b'\n':
+            o.write('\n')
+            nextbyte = i.read(1)
+
+        # case 2
+        elif coded_int == 249:
+            # There will be two byes of encoded chars
+            nextbyte = i.read(1)
+            if int.from_bytes(nextbyte, sys.byteorder) + 121 > len(wordlist):
+                # new word
+                nextbyte = i.read(1)
+                word = ''
+                while ( 
+                    (int.from_bytes(nextbyte, sys.byteorder) < 129) 
+                    & (nextbyte != b'') & (nextbyte != b'\n')
+                  ): # Read until another symbol char, or a newline, or EOF
+
+                    word += nextbyte.decode('ascii')
+                    nextbyte = i.read(1)
+
+                o.write(word)
+                if (nextbyte != b'') & (nextbyte != b'\n'):
+                    o.write(' ') # Add a space only between words
+                wordlist.append(word)
+            else:
+                # not new word
+                n = len(wordlist) - (int.from_bytes(nextbyte, sys.byteorder) + 121)
+                word = wordlist[n]
+                wordlist.remove(word)
+                wordlist.append(word)
+                o.write(word)
+                nextbyte = i.read(1)
+                if (nextbyte != b'') & (nextbyte != b'\n'):
+                    o.write(' ') # Add a space only between words
+
+        # Case 3
+        elif coded_int == 250:
+            # Three bytes of encoded chars
+            print("case 3")
+            nextbyte = i.read(1)
+            code = int.from_bytes(nextbyte, sys.byteorder) * 256
+            print("code byte 1: " + str(nextbyte))
+            nextbyte = i.read(1)
+            code += int.from_bytes(nextbyte, sys.byteorder)
+            nextbyte = i.read(1)
+            print(code)
+
+            if code > len(wordlist):
+                # New word
+                while ( 
+                    (int.from_bytes(nextbyte, sys.byteorder) < 129) 
+                    & (nextbyte != b'') & (nextbyte != b'\n')
+                  ): # Read until another symbol char, or a newline, or EOF
+
+                    word += nextbyte.decode('ascii')
+                    nextbyte = i.read(1)
+
+                o.write(word)
+                if (nextbyte != b'') & (nextbyte != b'\n'):
+                    o.write(' ') # Add a space only between words
+                wordlist.append(word)
+            else: 
+                # Not new word
+                n = len(wordlist) - (code + 1)
+                print("attempting to access wordlist index " + str(n))
+                word = wordlist[n]
+                wordlist.remove(word)
+                wordlist.append(word)
+                o.write(word)
+                nextbyte = i.read(1)
+                if (nextbyte != b'') & (nextbyte != b'\n'):
+                    o.write(' ') # Add a space only between words
+
+        # Case 1
+        else:
+            if coded_int - 128 > len(wordlist):
+                # Word we haven't yet encountered
+                nextbyte = i.read(1)
+                word = ''
+
+                while ( 
+                        (int.from_bytes(nextbyte, sys.byteorder) < 129) 
+                        & (nextbyte != b'') & (nextbyte != b'\n')
+                      ): # Read until another symbol char, or a newline, or EOF
+
+                        word += nextbyte.decode('ascii')
+                        nextbyte = i.read(1)
+
+                o.write(word)
+                if (nextbyte != b'') & (nextbyte != b'\n'):
+                    o.write(' ') # Add a space only between words
+                wordlist.append(word)
+            else:
+                # Word already encountered
+                # Resolve symbol into index on wordlist
+                n = len(wordlist) - (int.from_bytes(nextbyte, sys.byteorder) - 128)
+                print("attempting to access wordlist index " + str(n))
+                word = wordlist[n]
+                wordlist.remove(word) # Do the MTF stuff
+                wordlist.append(word)
+                o.write(word)
+                nextbyte = i.read(1) 
+                if (nextbyte != b'') & (nextbyte != b'\n'):
+                    o.write(' ') # Add a space only between words
+
+
+
+        '''
+        elif int.from_bytes(nextbyte, sys.byteorder) - 128 > len(wordlist):
+            # Word we haven't yet encountered
+            nextbyte = i.read(1)
+            word = ''
+
+            while ( 
+                    (int.from_bytes(nextbyte, sys.byteorder) < 129) 
+                    & (nextbyte != b'') & (nextbyte != b'\n')
+                  ): # Read until another symbol char, or a newline, or EOF
+
+                    word += nextbyte.decode('ascii')
+                    nextbyte = i.read(1)
+
+            o.write(word)
+            if (nextbyte != b'') & (nextbyte != b'\n'):
+                o.write(' ') # Add a space only between words
+            wordlist.append(word) '''
+
+        '''
+        else:
+            # Word already encountered
+            # Resolve symbol into index on wordlist
+            n = len(wordlist) - (int.from_bytes(nextbyte, sys.byteorder) - 128)
+            word = wordlist[n]
+            wordlist.remove(word) # Do the MTF stuff
+            wordlist.append(word)
+            o.write(word)
+            nextbyte = i.read(1) 
+            if (nextbyte != b'') & (nextbyte != b'\n'):
+                o.write(' ') # Add a space only between words
+        '''
+    i.close()
+    o.close()
+    sys.exit()
