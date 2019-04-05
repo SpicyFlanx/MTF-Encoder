5a056b4d7047d78e77af0009d9b382bb60d3b9c4
Mattias Park <mattiaspark@uvic.ca>
2019-04-04 22:23:01 -0700

    this is what you get for not backing up your work

---
+++ a4/coding2.c
@@ -0,0 +1,22 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char  magic_number_1[] = {0xBA, 0x5E, 0xBA, 0x11, 0x00};
+char  magic_number_2[] = {0xBA, 0x5E, 0xBA, 0x12, 0x00};
+
+
+
+/*
+ * The encode() function may remain unchanged for A#4.
+ */
+
+int encode(FILE *input, FILE *output) {
+    return 0;
+}
+
+
+int decode(FILE *input, FILE *output) {
+    return 0;
+}
