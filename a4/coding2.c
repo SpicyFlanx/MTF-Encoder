63e141247c5cd98a6be956ddc1fe74c456a37aae
Mattias Park <mattiaspark@uvic.ca>
2019-04-07 23:19:59 -0700

    invest in straws

--- a4/coding2.c
+++ a4/coding2.c
@@ -18,5 +18,47 @@ int encode(FILE *input, FILE *output) {
 
 
 int decode(FILE *input, FILE *output) {
-    return 0;
+    
+    // Check magicnum
+    char read_magicnum[5];
+    for (int i=0;i<5;i++) {
+    	read_magicnum[i] = fgetc( FILE *input );
+    }
+
+    // If magicnum not found, print an error
+    if (strcmp(read_magicnum, magic_number_1) != 0 
+    	&& strcmp(read_magicnum, magic_number_2) != 0) {
+    	fprintf(stderr, "Error: magic number not found!");
+    	exit(1);
+    }
+
+    // read
+
+    // Decoder loop
+    // While there's more to be read:
+
+    	// nextbyte = read byte
+
+    	// deal with newline
+
+    	// Case 2
+
+    		// New word
+
+    		// Not a new word
+
+    	// Case 3
+
+    		// New word
+
+    		// Not a new word
+
+    	// Case 1
+
+    		// New word
+
+    		// not a new word
+
+	return 0;
+
 }
